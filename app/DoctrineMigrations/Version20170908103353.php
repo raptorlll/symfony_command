faec5b33eb21b11e608fa3afdb5ed487dedf9036
leonpolq <leonpolq@gmail.com>
2017-09-08 13:55:36 +0300

    Forgotten Migration

---
+++ app/DoctrineMigrations/Version20170908103353.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170908103353 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `tblProductData` CHANGE floatCost decimalCost DECIMAL (10,2) NOT NULL;');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `tblProductData` CHANGE decimalCost floatCost FLOAT NOT NULL;');
+    }
+}
